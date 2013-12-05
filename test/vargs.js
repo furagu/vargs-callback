6b4ff16d76e8ca48418b3d7bad11c73ddac531e9
furagu <frag.trials@gmail.com>
2013-12-05 16:36:04 +0800

    Added two more tests

--- test/vargs.js
+++ test/vargs.js
@@ -17,7 +17,7 @@ describe('vargs', function () {
         function foo(a, b, c) {
             should.strictEqual(a ? 1 : undefined, a)
             should.strictEqual(b ? 2 : undefined, b)
-            c.should.be.type('function')
+            should(c).be.type('function')
         }
         foo = foo.vargs
 
@@ -26,7 +26,25 @@ describe('vargs', function () {
         foo(1, 2, function () {})
     })
 
-    it('should not do anything if the last argument is not a function')
+    it('should not modify arguments if the last argument is not a function', function () {
+        function foo(a, b, c) {
+            should(c).not.be.type('function')
+        }
+        foo = foo.vargs
 
-    it('should not do anything if the number of arguments is greater than stated in function definition')
+        foo()
+        foo(1)
+        foo(1, 2)
+        foo(1, 2, 3)
+        foo(function () {}, 1)
+    })
+
+    it('should not modify arguments if there are more than stated in the function definition', function () {
+        var cb = function () {}
+
+        ;(function (a, b, c) {
+            arguments.should.eql([1, 2, 3, cb])
+            ;[a, b, c].should.eql([1, 2, 3])
+        }.vargs(1, 2, 3, cb))
+    })
 })
