b40023f159b3e96a09249f014e43eb5def4eef49
Donovan <donovan.harscoet@gmail.com>
2014-06-04 09:49:01 +0200

    Add optional argument to vargs to be able to force existing callback

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function vargs(fn) {
+function vargs(fn, requiredCallback) {
     var slice = Array.prototype.slice,
         paramsCount = fn.length
 
     return function () {
         var argsCount = arguments.length,
             lastArg = arguments[argsCount - 1]
+        if (requiredCallback && typeof lastArg !== 'function')
+            throw new Error('callback is required')
 
         if (argsCount >= paramsCount || typeof lastArg !== 'function')
             return fn.apply(this, arguments)
